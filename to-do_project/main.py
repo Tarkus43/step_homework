69e1386e997d834a5be39252c9fd211eba5ec3ad
Tarkus43 <jeka.spets333@gmail.com>
2025-05-14 13:24:44 +0200

    reworked add func

--- to-do_project/main.py
+++ to-do_project/main.py
@@ -7,6 +7,13 @@ STATUS = {
     HIGH: "high"
 }
 
-def add_to_db(text):
+def add_todo(name:str, text:str,prio:int,status) -> None:
+    with open('db.txt','r') as db:
+        id = None
+        for line in db:
+            id = int(line) + 1 
+            break
     with open('db.txt','a') as db:
-        db.write('\n' + text)
+        db.write('\n' + f'{name}/{text}/{id}/{prio}/{status}')
+
+add_todo('new todo','im gonna to test this func',HIGH,STATUS[HIGH],)                    
