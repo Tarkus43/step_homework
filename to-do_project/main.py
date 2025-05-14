3b6f159cf8964b53f2853edf4396b8e241ea1cff
Tarkus43 <jeka.spets333@gmail.com>
2025-05-14 15:36:17 +0200

    added func that gets last id

--- to-do_project/main.py
+++ to-do_project/main.py
@@ -16,4 +16,12 @@ def add_todo(name:str, text:str,prio:int,status) -> None:
     with open('db.txt','a') as db:
         db.write('\n' + f'{name}/{text}/{id}/{prio}/{status}')
 
-add_todo('new todo','im gonna to test this func',HIGH,STATUS[HIGH],)                    
+def get_last_id() -> int:
+    max_id = 0
+    with open('db.txt','r') as db:
+        for line in db:
+            if int(line.split('/')[2]) > int(max_id):
+                max_id = line.split('/')[2]
+    return max_id            
+
+print(get_last_id())                
