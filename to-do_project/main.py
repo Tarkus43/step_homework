0d10de75ab8317fd92c7ca5a19cd5998320c8bae
Tarkus43 <jeka.spets333@gmail.com>
2025-05-19 11:46:21 +0200

    added new func that formates todo from str to dict and added some consts

--- to-do_project/main.py
+++ to-do_project/main.py
@@ -1,3 +1,4 @@
+import copy
 LOW = "1"
 MEDIUM = "2"
 HIGH = "3"
@@ -6,6 +7,8 @@ STATUS = {
     MEDIUM: "medium",
     HIGH: "high"
 }
+TODO_DICT = {'name':None, 'descr':None, 'id':None, 'prio':None, 'status':None}
+KEYS = ['name', 'descr', 'id', 'prio', 'status']
 
 def add_todo(name:str, text:str,prio:int,status) -> None:
     with open('db.txt','r') as db:
@@ -22,6 +25,11 @@ def get_last_id() -> int:
         for line in db:
             if int(line.split('/')[2]) > int(max_id):
                 max_id = line.split('/')[2]
-    return max_id            
+    return max_id               
 
-print(get_last_id())                
+def todo_to_dict(todo:str) -> dict:
+    todo_list = todo.split('/')
+    todo_dict = dict(zip(KEYS,todo_list))
+    return todo_dict
+
+print(todo_to_dict('new todo/im gonna to test this func/6/4/high'))
