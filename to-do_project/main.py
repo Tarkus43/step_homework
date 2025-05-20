4c099ebcd4e997c97f7bcb324561bcc71a11d56c
Tarkus43 <jeka.spets333@gmail.com>
2025-05-20 10:08:28 +0200

    reworked get_last_id and solved todo_to_dict id int problem

--- to-do_project/main.py
+++ to-do_project/main.py
@@ -8,21 +8,13 @@ STATUS = {
     HIGH: "high"
 }
 TODO_DICT = {'name':None, 'descr':None, 'id':None, 'prio':None, 'status':None}
-KEYS = ['name', 'descr', 'id', 'prio', 'status']
-
-# !!!NEEDS AN UPDATE!!! gets highest id to find last todo
-def get_last_id() -> int:
-    max_id = 0
-    with open('db.txt','r') as db:
-        for line in db:
-            if int(line.split('/')[2]) > int(max_id):
-                max_id = line.split('/')[2]
-    return max_id               
+KEYS = ['name', 'descr', 'id', 'prio', 'status']             
 
 # changes todo from string format to dict format so python can easily work with it
 def todo_to_dict(todo:str) -> dict:
     todo_list = todo.split('/')
     todo_dict = dict(zip(KEYS,todo_list))
+    todo_dict['id'] = int(todo_dict['id'])
     return todo_dict
 
 # changes todo in dict format to string format for reading or adding to txt file
@@ -34,5 +26,14 @@ def add_todo(todo:dict) -> None:
     with open('db.txt','a') as db:
         db.write('\n' + dict_to_todo(todo))
 
-add_todo(todo_to_dict('new todo/im gonna to test this func/8/4/high'))
+# gets highest id to find last todo
+def get_last_id() -> int:
+    max_id = 0
+    with open('db.txt','r') as db:
+        for line in db:
+            if todo_to_dict(line)['id'] > int(max_id):
+                max_id = line.split('/')[2]
+    return max_id  
+
+print(get_last_id())
 
