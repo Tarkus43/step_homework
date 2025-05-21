c8b0a60698453f73cf18d300fb4628cf965ef374
Tarkus43 <jeka.spets333@gmail.com>
2025-05-21 10:45:13 +0200

    added changing function and solved dict to todo problem getting int instead of str

--- to-do_project/main.py
+++ to-do_project/main.py
@@ -8,7 +8,8 @@ STATUS = {
     HIGH: "high"
 }
 TODO_DICT = {'name':None, 'descr':None, 'id':None, 'prio':None, 'status':None}
-KEYS = ['name', 'descr', 'id', 'prio', 'status']             
+KEYS = ['name', 'descr', 'id', 'prio', 'status']
+EXAMPLE = 'name/description of todo/1/3/high'             
 
 # changes todo from string format to dict format so python can easily work with it
 def todo_to_dict(todo:str) -> dict:
@@ -19,7 +20,7 @@ def todo_to_dict(todo:str) -> dict:
 
 # changes todo in dict format to string format for reading or adding to txt file
 def dict_to_todo(todo:dict[str:str,str:str,str:int,str:int,str:str]) -> str:
-    return '/'.join([x for x in todo.values()])
+    return '/'.join([str(x) for x in todo.values()])
 
 # adds todo to txt file
 def add_todo(todo:dict) -> None:
@@ -35,7 +36,7 @@ def get_last_id() -> int:
                 max_id = line.split('/')[2]
     return int(max_id)  
 
-# gets todo in str type
+# gets todo in str type by id
 def get_todo(id:int) -> str:
     if id > get_last_id():
         return 'ERROR: id does not exist'
@@ -60,5 +61,22 @@ def find_todo(key_word:str) -> list[dict]:
     else:
         return todo_list
 
-print(find_todo('func'))            
+def change_todo(id: int, param: str, text: str) -> str:
+    todo_list = []
+    with open('db.txt', 'r') as file:
+        for line in file:
+            todo_list.append(todo_to_dict(line.strip()))
+
+    for todo in todo_list:
+        if todo['id'] == id:
+            todo[param] = text
+            break
+
+    with open('db.txt', 'w') as file:
+        for todo in todo_list:
+            file.write(dict_to_todo(todo) + '\n')
+
+
+change_todo(1,'descr','i tested this shit nigga')    
+
 
